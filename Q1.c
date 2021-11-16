84a5b297c1bc063c2dde060365937669d9d9a998
GeorginReji <georginreji@gmail.com>
2021-11-16 14:55:24 +0530

    Q1 changes

--- Q1.c
+++ Q1.c
@@ -20,15 +20,14 @@ int main()
     unsigned int n;
     printf("enter the integer\n");
     scanf("%d", &n);
-
     int count = 0;
 
-    while (n == 0)
+    while (n != 0)
     {
-        int r = n / 10;
-        if (r > 1)
-            count++;
-        n = n * 10;
+        int r = n % 2;
+        if (r == 1)
+            count++ ;
+        n = n / 2;
     }
 
     printf("no of bits those are 1 ");
